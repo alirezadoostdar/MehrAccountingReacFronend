d91e8b32b1657e697cf9fe6438ef6d1484e82a66
alirezadoostdar <alireza.doostdar@gmail.com>
2026-08-09 15:40:16 +0200

    refactor login form

--- src/Services/users/UserService.ts
+++ src/Services/users/UserService.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from 'axios'
+import { AxiosError, AxiosResponse } from 'axios'
 import { publicApi } from '@/Services/apiClient'
 import type { LoginResponse } from '@/Services/users/types/loginResponse'
 import type { apiResponse } from './types/apiResponse'
@@ -12,10 +12,29 @@ export async function login(loginDto: LoginRequest): Promise<LoginResponse> {
       loginDto
     )
 
-    console.log('Login response:', res)
-  } catch (error) {
-    console.error('Login request failed:', res)
-    throw error
+    console.log('Login response:', res.data)
+    if (res.status === 200 && res.data.isSuccess) {
+      return {
+        isAuthenticated: true,
+        token: res.data.value,
+        error: '',
+      }
+    } else {
+      return {
+        isAuthenticated: false,
+        token: '',
+        error: res.data.errorMessage,
+      }
+    }
+  } catch (err) {
+    console.log('RAW ERROR:', err)
+    console.log('IS AXIOS ERROR:', err instanceof AxiosError)
+    if (err instanceof AxiosError) {
+      console.log('STATUS:', err.response?.status)
+      console.log('DATA:', err.response?.data)
+      console.log('HEADERS:', err.response?.headers)
+    }
+    throw err
   }
 
   const response: LoginResponse = {
