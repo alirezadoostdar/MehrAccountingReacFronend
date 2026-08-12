c3d35dd0a1b5d93fbdbb5d10d28ef10f117fe49b
alirezadoostdar <alireza.doostdar@gmail.com>
2026-08-12 21:04:48 +0200

    refactor for add token to api

--- src/Services/apiClient.ts
+++ src/Services/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { authStore } from '@/stores/auth-store'
 
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'http://localhost:5161/api'
@@ -12,7 +13,8 @@ export const apiClient = axios.create({
 })
 
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
+  const token = authStore.getState().auth.accessToken
+  console.log('token:', token)
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
--- src/Services/contacts/ContactService.ts
+++ src/Services/contacts/ContactService.ts
@@ -1,12 +1,19 @@
 import { useQuery } from '@tanstack/react-query'
+import { useAuthStore } from '@/stores/auth-store'
+import { apiClient } from '../apiClient'
 import type { ContactType } from '../pages/contacts/types/ContactTypes'
 import type { ApiResponse } from './ApiResponse'
 import { publicApi } from './apiClient'
+import type { ContactListItem } from './types/contactListItem'
 
 export const useContactTypes = () => {
   return useQuery({
     queryKey: ['contactTypes'],
     queryFn: async () => {
+      const token = useAuthStore.getState().auth.accessToken
+      console.log('token:', token)
+      /*       const list = await getContacts()
+      console.log('Fetched contacts:', list) */
       const res =
         await publicApi.get<ApiResponse<ContactType[]>>('/contact/state')
       return res.data.value
@@ -15,6 +22,6 @@ export const useContactTypes = () => {
 }
 
 export async function getContacts(): Promise<ContactListItem[]> {
-  const res = await publicApi.get<ApiResponse<ContactListItem[]>>('/contacts')
+  const res = await apiClient.get<ApiResponse<ContactListItem[]>>('/contacts')
   return res.data.value
 }
