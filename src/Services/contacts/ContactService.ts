b613186cbe3d57c8d03cfe81bd65243045d944f0
alirezadoostdar <alireza.doostdar@gmail.com>
2026-08-21 23:11:13 +0200

    refactor contact service

--- src/Services/contacts/ContactService.ts
+++ src/Services/contacts/ContactService.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { apiClient } from '../apiClient'
+import type { PagedResult } from '../commenTypes/PagedResult'
 import type { ApiResponse } from './ApiResponse'
 import type { ContactListItem } from './types/contactListItem'
 
@@ -7,8 +8,16 @@ export const getContacts = () => {
   return useQuery({
     queryKey: ['contactList'],
     queryFn: async () => {
-      const res =
-        await apiClient.get<ApiResponse<ContactListItem[]>>('/contact')
+      const res = await apiClient.get<
+        ApiResponse<PagedResult<ContactListItem>>
+      >('/contact', {
+        params: {
+          page: 1,
+          pageSize: 10,
+          search: '',
+        },
+      })
+      console.log('res.data.value', res.data.value)
       return res.data.value
     },
   })
