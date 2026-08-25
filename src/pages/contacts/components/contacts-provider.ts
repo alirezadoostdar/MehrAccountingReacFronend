2c272eb203db782e73d404bd495af8ade76e0820
alirezadoostdar <alireza.doostdar@gmail.com>
2026-08-25 22:02:09 +0200

    change contact providr

--- src/pages/contacts/components/contacts-provider.ts
+++ src/pages/contacts/components/contacts-provider.ts
@@ -1,35 +1,36 @@
-import React, { useState } from "react"
-import useDialogState from "@/hooks/use-dialog-state"
-import type { ContactListItem } from "@/Services/contacts/types/contactListItem"
+import React, { useState } from 'react'
+import useDialogState from '@/hooks/use-dialog-state'
+import { type User } from '../data/schema'
 
-
-type ContactDialogType = 'add' | 'edit' | 'delete'
+type ContactsDialogType = 'invite' | 'add' | 'edit' | 'delete'
 
 type ContactsContextType = {
-    open: ContactDialogType | null
-    setOpen: (str: ContactDialogType | null) => void
-    currentRow: ContactListItem | null
-    setCurrentRow: React.Dispatch<React.SetStateAction<ContactListItem | null>>
+  open: ContactsDialogType | null
+  setOpen: (str: ContactsDialogType | null) => void
+  currentRow: User | null
+  setCurrentRow: React.Dispatch<React.SetStateAction<User | null>>
 }
 
 const ContactsContext = React.createContext<ContactsContextType | null>(null)
 
 export function ContactsProvider({ children }: { children: React.ReactNode }) {
-    const [open, setOpen] = useDialogState<ContactDialogType>(null)
-    const [currentRow, setCurrentRow] = useState<ContactListItem | null>(null)
+  const [open, setOpen] = useDialogState<ContactsDialogType>(null)
+  const [currentRow, setCurrentRow] = useState<User | null>(null)
 
-    return(
-        <ContactsContext value = {{open,setOpen,currentRow,setCurrentRow}}>
-            {children}
-        </ContactsContext>
-    )
-
+  return (
+    <ContactsContext value={{ open, setOpen, currentRow, setCurrentRow }}>
+      {children}
+    </ContactsContext>
+  )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useContacts = () => {
-    const contactsContext = React.useContext(ContactsContext)
-    if (!contactsContext) {
-        throw new Error('useContacts has to be used within <ContactsContext>')
-    }
-    return contactsContext
+  const contactsContext = React.useContext(ContactsContext)
+
+  if (!contactsContext) {
+    throw new Error('useContacts has to be used within <ContactsContext>')
+  }
+
+  return contactsContext
 }
